14611794dd09a3e6917a19dbd4a805719f14a2c1
MARIAM ZHU <miriam.zhudong@gmail.com>
2021-07-29 00:23:42 +0800

    update

--- map.py
+++ map.py
@@ -1,53 +1,42 @@
 import folium
 import pandas as pd
-
-import pandas as pd
-from sodapy import Socrata
-
-
-client = Socrata("data.melbourne.vic.gov.au", None)
-
-
-CPBL = client.get("vh2v-4nfs", limit=2000)  #CPBL:CarParkBayLocation
-CPBL_df = pd.DataFrame.from_records(CPBL)
-
-CPBR = client.get("ntht-5rk7", limit=20000)#CPBR:Car Park Bay Restrictions
-CPBR_df = pd.DataFrame.from_records(CPBR)
-
-
-#*****
-merge_df=pd.merge(CPBL_df,CPBR_df,left_on='bay_id',right_on='bayid', how='left')
-#merge_df.isnull().any()
-#merge_df.info()
-df=merge_df.iloc[:,[0,1,2,4,5,9,10,11,12,13,16,17,18]]
-df[['lat','lon','disabilityext1','duration1']].apply(pd.to_numeric)
+import os 
+import webbrowser
 
 def mark_map(data):
     """
-    带有标注的地图
+    A map with markings
     :param data:
     :return:
     """
-    # 地图制作
+    # Map production
     myMap = folium.Map(location=[-37.82, 144.95], zoom_start=14)
 
     for i in range(len(data)):
-        # 自定义 popup 内容
+        # Customize popup content
         test = folium.Html(
             '<b>bay_id:{}</b></br> <b>statues:{}</b></br> <b>description:{}</b></br> <b>effectiveonph:{}</b></br> '.format(data.iloc[i]['bay_id'], data.iloc[i]['status'], data.iloc[i]['description1'],data.iloc[i]['effectiveonph1']),
             script=True)
         
         popup = folium.Popup(test)
         
-        if df.iloc[i]['status']=='Unoccupied':
+        if data.iloc[i]['status']=='Unoccupied':
             icon=folium.Icon(color='red', icon='info-sign')
         else:
             icon=folium.Icon(color='blue', icon='ok-sign')
         
         folium.Marker([data.iloc[i]['lat'], data.iloc[i]['lon']], popup=popup,icon=icon).add_to(myMap)
 
-    # 保存地图
-    myMap.save('testMap.html')
+    # Save the map
+    geo_name = 'Geo_Map.html'
+    #to save it in a file
+    myMap.save('images/'+geo_name)
+    webbrowser.open('images/'+geo_name)
+
+    image_path = "file://"+os.getcwd()+ '/'+ 'images/'+geo_name
+    webbrowser.open(image_path)
+    
+    #myMap.save('testMap.html')
 
 #if __name__ == '__main__':
-mark_map(df)
+#mark_map(df)
